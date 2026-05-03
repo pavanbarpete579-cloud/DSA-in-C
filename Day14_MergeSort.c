9a27e609f95d73ce1056d02576b1884bd10a4a33
Pavan Barpete <pavanbarpete579@gmail.com>
2026-05-03 07:28:25 +0530

    Day14 c: merge sort and quick sort

---
+++ Day14_MergeSort.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void merge(int arr[], int l, int m, int r) {
+    int i = l, j = m + 1, k = 0;
+    int temp[100];
+
+    while (i <= m && j <= r) {
+        if (arr[i] < arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+
+    while (i <= m)
+        temp[k++] = arr[i++];
+
+    while (j <= r)
+        temp[k++] = arr[j++];
+
+    for (i = l, k = 0; i <= r; i++, k++)
+        arr[i] = temp[k];
+}
+
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = (l + r) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+int main() {
+    int arr[] = {5, 2, 4, 1, 3};
+    int n = 5;
+
+    mergeSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
