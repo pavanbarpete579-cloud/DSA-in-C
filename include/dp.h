18c729e9e26214b56fb9814646a0fbd31a4f2ee7
Pavan Barpete <pavanbarpete579@gmail.com>
2026-05-09 23:06:40 +0530

    Added longest common subsequence using dynamic programing

--- include/dp.h
+++ include/dp.h
@@ -2,5 +2,7 @@
 #define DP_H
 
 int fibonacciMemo(int n);
+int knapsack(int capacity, int weights[], int values[], int n);
+int longestCommonSubsequence(char str1[], char str2[]);
 
 #endif
--- src/dp.c
+++ src/dp.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "../include/dp.h"
-
+#include <string.h>
 int memo[100];
 
 int fibonacciMemoHelper(int n)
@@ -31,11 +31,86 @@ int fibonacciMemo(int n)
 
     return fibonacciMemoHelper(n);
 }
+
+int max(int a, int b)
+{
+    return (a > b) ? a : b;
+}
+
+int knapsack(int capacity, int weights[], int values[], int n)
+{
+    int dp[100][100];
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int w = 0; w <= capacity; w++)
+        {
+            if (i == 0 || w == 0)
+            {
+                dp[i][w] = 0;
+            }
+            else if (weights[i - 1] <= w)
+            {
+                dp[i][w] =
+                    max(
+                        values[i - 1] +
+                        dp[i - 1][w - weights[i - 1]],
+
+                        dp[i - 1][w]
+                    );
+            }
+            else
+            {
+                dp[i][w] = dp[i - 1][w];
+            }
+        }
+    }
+
+    return dp[n][capacity];
+}
+int maxValue(int a, int b)
+{
+    return (a > b) ? a : b;
+}
+
+int longestCommonSubsequence(char str1[], char str2[])
+{
+    int m = strlen(str1);
+    int n = strlen(str2);
+
+    int dp[100][100];
+
+    for (int i = 0; i <= m; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            if (i == 0 || j == 0)
+            {
+                dp[i][j] = 0;
+            }
+            else if (str1[i - 1] == str2[j - 1])
+            {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else
+            {
+                dp[i][j] =
+                    maxValue(dp[i - 1][j],
+                             dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[m][n];
+}
 void dpMenu()
 {
-    int n = 10;
+    char str1[] = "ABCBDAB";
 
-    int result = fibonacciMemo(n);
+    char str2[] = "BDCAB";
 
-    printf("Fibonacci(%d) = %d\n", n, result);
+    int length =
+        longestCommonSubsequence(str1, str2);
+
+    printf("LCS Length = %d\n", length);
 }
